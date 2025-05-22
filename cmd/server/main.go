1ec1ab494b1e1f43ee32dcba841e46996dbb6743
Dilgo-dev <dilgopierre@gmail.com>
2025-05-22 03:49:47 +0200

    feat: Load environment variables and configure server port dynamically

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/Dilgo-dev/quotesly/internal/config"
 	"github.com/Dilgo-dev/quotesly/internal/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+		log.Println("PORT is not set, using default port 3000")
+	}
+
 	_, err := config.NewDatabase()
 	if err != nil {
 		log.Fatal(err)
@@ -21,5 +31,6 @@ func main() {
 
 	r.Get("/", handlers.GetQuote)
 
-	http.ListenAndServe(":3000", r)
+	log.Println("Starting server on port http://localhost:" + port)
+	http.ListenAndServe(":"+port, r)
 }
