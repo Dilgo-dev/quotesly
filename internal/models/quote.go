96a40628194def80f7dea4ad7500888aebb760cf
Dilgo-dev <dilgopierre@gmail.com>
2025-05-22 04:12:32 +0200

    feat: Add Category field to Quote model for improved categorization

--- internal/models/quote.go
+++ internal/models/quote.go
@@ -6,4 +6,5 @@ type Quote struct {
 	gorm.Model
 	Quote     string `gorm:"unique;not null;type:varchar(255);default:null"`
 	Author    string `gorm:"not null;type:varchar(100);default:null"`
+	Category  string `gorm:"not null;type:varchar(100);default:null"`
 }
